7bfa7e4f9620fe4103b1932fde65d692a9b1a24c
Neko205 <neko200553@gmail.com>
2024-08-15 06:44:35 -0700

    修改：     main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import time
 import requests
 import json
-
+import os
 """
     
 """
 
 def getKeyAndip():
-    ip = ""
-    key = ""
+    # ip = ""
+    # key = ""
+    ip = os.getenv('URL')
+    key = os.getenv('URLKEY')
     return ip,key
 # 获取时间戳 当前时间点往前24小时
 def timeStart():
@@ -17,13 +19,34 @@ def timeStart():
     newday = int(newday)
     return oldday,newday
 def main():
+    user_input = input("选择所获取的数据：1 获取攻击数据 2 获取服务器状态")
+    if user_input == "1":
+
+        json_date = json.loads(apiGetHackip())
+        if json_date['response_code'] == 0:
+            print("获取成功")
+            Hackips = json_date['data']['attack_ip']
+            print(Hackips)
+            # print(json_date)
+        else:
+            print("获取失败")
+            # print(json_date)
+            
+    elif user_input == "2":
+        print("获取服务器状态")
+    else:
+        print("输入错误")
+
+
+def apiGetHackip():
     ip,key = getKeyAndip()
     oldday,newday = timeStart()
+
     # 获取攻击数据
     url = f"https://{ip}/api/v1/attack/ip?api_key={key}"
     print(url)
     payload = json.dumps({
-      "start_time": oldday,
+     "start_time": oldday,
      "end_time": newday,
      "intranet": 0,
      "threat_label": [
@@ -33,10 +56,10 @@ def main():
     headers = {
       'Content-Type': 'application/json'
     }
+
+    # 忽略证书验证
     response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-    print(response.text)
-    echo = response.text
-    print(echo)
+    return response.text
 
 if __name__ == '__main__':
     main()
